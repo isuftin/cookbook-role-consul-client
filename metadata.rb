503546688c605e02c5e69042c831a83e2795c09b
isuftin <isuftin@usgs.gov>
2015-02-06 07:22:08 -0600

    Updated to next Consul version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'isuftin@usgs.gov'
 license          'Public Domain'
 description      'Role cookbook for the Consul client install'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'consul'
